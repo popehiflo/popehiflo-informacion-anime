8b87efe638f596f143fa7f58ab741719d918cbef
popehiflo <poolpetterhijuel@hotmail.com>
2021-02-24 09:35:30 -0500

    Se crea microservicio anime

---
+++ backend/server-spring/micrserv-anime/src/test/java/com/popehiflo/informacionanimes/micrservanime/MicrservAnimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.popehiflo.informacionanimes.micrservanime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrservAnimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
