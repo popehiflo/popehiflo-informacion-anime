ae58141ae58c8e51393a875b3f9baa5414dc54f0
popehiflo <poolpetterhijuel@hotmail.com>
2021-02-25 09:20:09 -0500

    Se configura .properties, conexion db, puerto, etc

--- backend/server-spring/micrserv-anime/src/main/java/com/popehiflo/informacionanimes/anime/MicrservAnimeApplication.java
+++ backend/server-spring/micrserv-anime/src/main/java/com/popehiflo/informacionanimes/anime/MicrservAnimeApplication.java
@@ -1,4 +1,4 @@
-package com.popehiflo.informacionanimes.micrservanime;
+package com.popehiflo.informacionanimes.anime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
