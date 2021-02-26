806f5bbba85108ead5d08e2b18d21c83a1d39457
popehiflo <poolpetterhijuel@hotmail.com>
2021-02-26 09:19:43 -0500

    Se implementa el resto de RequestMethods para CRUD

--- backend/server-spring/micrserv-anime/src/main/java/com/popehiflo/informacionanimes/anime/controller/AnimeController.java
+++ backend/server-spring/micrserv-anime/src/main/java/com/popehiflo/informacionanimes/anime/controller/AnimeController.java
@@ -3,8 +3,14 @@ package com.popehiflo.informacionanimes.anime.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,4 +39,35 @@ public class AnimeController {
 		
 		return ResponseEntity.ok(animesConsultados);
 	}
+	
+	// http://localhost:8080/anime/guardarAnime
+	@PostMapping
+	@RequestMapping(value = "guardarAnime", method = RequestMethod.POST)
+	public ResponseEntity<?> guardarAnime(@RequestBody Anime anime) {
+		
+		Anime animeGuardado = this.animeServiceImpl.guardarAnime(anime);
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(animeGuardado);
+	}
+	
+	// http://localhost:8080/anime/actualizarAnime
+	@PutMapping
+	@RequestMapping(value = "actualizarAnime", method = RequestMethod.PUT)
+	public ResponseEntity<?> actualizarAnime(@RequestBody Anime anime) {
+		
+		Anime animeActualizado = this.animeServiceImpl.actualizarAnime(anime);
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(animeActualizado);
+	}
+	
+	// http://localhost:8080/anime/eliminarAnime/5
+	@DeleteMapping
+	@RequestMapping(value = "eliminarAnime/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<?> eliminarAnime(@PathVariable Integer id) {
+		
+		this.animeServiceImpl.eliminarAnime(id);
+		
+		return ResponseEntity.ok().build();
+	}
+	
 }
