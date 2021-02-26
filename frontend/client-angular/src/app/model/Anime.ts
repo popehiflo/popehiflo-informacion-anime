df49b1ca6cd1a85027a8ed85ebacf9be8b0cd262
popehiflo <poolpetterhijuel@hotmail.com>
2021-02-26 15:22:21 -0500

    Se crea entidad Anime, mapeara response microserv

---
+++ frontend/client-angular/src/app/model/Anime.ts
@@ -0,0 +1,24 @@
+/**
+ * Entidad que mapea la informacion
+ * obtenida del microservicio Anime
+ */
+export class Country {
+
+  /**
+   * Identificador del anime
+   */
+  public id: number;
+  /**
+   * Nombre del anime
+   */
+  public nombre: string;
+  /**
+   * Anio del anime
+   */
+  public anio: number;
+  /**
+   * Fecha de creacion del anime
+   */
+  public fechaCreacion: Date;
+
+}
