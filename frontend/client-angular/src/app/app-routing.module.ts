9a8b2ba1b6940b2c819dd454ac55399a89c81d21
popehiflo <poolpetterhijuel@hotmail.com>
2021-02-27 00:04:51 -0500

    Se implementa rutas

--- frontend/client-angular/src/app/app-routing.module.ts
+++ frontend/client-angular/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AnimesComponent } from './components/animes/animes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'animes', pathMatch: 'full' },
+  { path:'animes', component: AnimesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
