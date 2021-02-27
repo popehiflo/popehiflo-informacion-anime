3f9ccfc63210e68b5a34d5cb67b0dfabf7099e67
popehiflo <poolpetterhijuel@hotmail.com>
2021-02-27 00:06:36 -0500

    Se agrega data y componente de prueba

--- frontend/client-angular/src/app/app.component.ts
+++ frontend/client-angular/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import COUNTRIES from './data/Countries';
+import { Country } from './model/Country';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'client-angular';
+
+  title = 'Ejemplo demo Table Countries';
+
+  page = 1;
+  pageSize = 4;
+  collectionSize = COUNTRIES.length;
+  countries: Country[];
+
+  constructor() {
+    this.refreshCountries();
+  }
+
+  refreshCountries() {
+    this.countries = COUNTRIES
+      .map((country, i) => ({id: i + 1, ...country}))
+      .slice((this.page - 1) * this.pageSize, (this.page - 1) * this.pageSize + this.pageSize);
+  }
 }
